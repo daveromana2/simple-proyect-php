4af0a08e102729eb6bbf9e50212d6abfdc08a2cb
Daniel Sanchez <daniel.sanchez@ti-synergy.com>
2015-04-24 15:30:52 -0500

    First commit

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+$pageTitle = "Contact Mike";
+$section = "contact";
+include ('inc/header.php');
+?>
+
+<div class="section page">
+    <div class="wrapper">
+        <h1>Contact</h1>
+        
+        <p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p>
+    </div>
+</div>
+
+<?php
+include 'inc/footer.php';
