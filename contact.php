2a4683243ba921baea98c23d7bfdbea44cb6c5c3
Daniel Sanchez <daniel.sanchez@ti-synergy.com>
2015-04-30 13:39:10 -0500

    Final revision for mini project

--- contact.php
+++ contact.php
@@ -1,12 +1,45 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+    $name = trim($_POST["name"]);
+    $email = trim($_POST["email"]);
+    $message = trim($_POST["message"]);
 
-    $email_body = "Name: " . $name . " \nEmail:" . $email . " \nMessage:" . $message;
+    if ($name == "" OR $email = "" OR $message = "") {
+        echo "You must specify a value for name.";
+        exit;
+    }
 
-    //TODO: Send Email
+    foreach ($_POST as $value) {
+        if (stripos($value, 'Content-Type') != FALSE) {
+            echo "There was a problem with the information you entered.";
+            exit;
+        }
+    }
+
+    if ($_POST["address"] != "") {
+        echo "Your submission has an error.";
+        exit;
+    }
+
+    require_once './inc/phpmailer/class.phpmailer.php';
+
+    $mail = new PHPMailer();
+    if (!$mail->validateAddress($email)) {
+        echo "You must specify a valid email address";
+        exit;
+    }
+
+    $email_body = "Name: " . $name . " <br> Email:" . $email . " <br> Message:" . $message;
+
+    $mail->isSendmail();
+    $mail->setFrom($email, $name);
+    $mail->addAddress('orders@shirts4mike.com', 'Shirts 4 Mike');
+    $mail->Subject = 'Shirts 4 Mike Contact Form Submission | ' . $name;
+    $mail->msgHTML($email_body);
+    //send the message, check for errors
+    if (!$mail->send()) {
+        echo "There was a problem sending the email: " . $mail->ErrorInfo;
+    } 
 
     header("Location: contact.php?status=thanks");
     exit;
@@ -20,30 +53,33 @@ include ('inc/header.php');
 <div class="section page">
     <div class="wrapper">
         <h1>Contact</h1>
-        
-        <?php if(isset($_GET["status"]) && $_GET["status"] == "thanks") { ?>
+
+        <?php if (isset($_GET["status"]) && $_GET["status"] == "thanks") { ?>
             <p>Thanks for the email. I&rsquo;ll be in touch shortly.</p>
-        <?php } else {?>
+        <?php } else { ?>
 
-        <p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p>
-        <form method="post" action="contact.php">
-            <table>
-                <tr>
-                    <th><label for="name">Name</label></th>
-                    <td><input type="text" name="name" id="name"></td>
-                </tr>
-                <tr>
-                    <th><label for="email">Email</label></th>
-                    <td><input type="email" name="email" id="email"></td>
-                </tr>
-                <tr>
-                    <th><label for="message">Message</label></th>
-                    <td><textarea name="message" id="message"></textarea></td>
-                </tr>
-
-            </table>
-            <input type="submit" value="Send" >
-        </form>
+            <p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p>
+            <form method="post" action="contact.php">
+                <table>
+                    <tr>
+                        <th><label for="name">Name</label></th>
+                        <td><input type="text" name="name" id="name"></td>
+                    </tr>
+                    <tr>
+                        <th><label for="email">Email</label></th>
+                        <td><input type="email" name="email" id="email"></td>
+                    </tr>
+                    <tr>
+                        <th><label for="message">Message</label></th>
+                        <td><textarea name="message" id="message"></textarea></td>
+                    </tr>
+                    <tr style="display: none;">
+                        <th><label for="address">Address</label></th>
+                        <td><input type="text" name="address" id="address"></td>
+                    </tr>
+                </table>
+                <input type="submit" value="Send" >
+            </form>
         <?php } ?>
     </div>
 </div>
