2d4f13eb066442834a07aae232f335558393e763
Daniel Sanchez <daniel.sanchez@ti-synergy.com>
2015-06-16 16:24:43 -0500

    some refactoring code

--- index.php
+++ index.php
@@ -1,50 +1,44 @@
-<?php 
-
+<?php
 require_once("inc/config.php");
-
+include(ROOT_PATH . "inc/products.php");
+$products = get_products_recent();
 $pageTitle = "Unique T-shirts designed by a frog";
 $section = "home";
-include(ROOT_PATH . 'inc/header.php'); ?>
-		<div class="section banner">
+include(ROOT_PATH . 'inc/header.php');
+?>
+<div class="section banner">
 
-			<div class="wrapper">
+    <div class="wrapper">
 
-				<img class="hero" src="<?php echo BASE_URL; ?>img/mike-the-frog.png" alt="Mike the Frog says:">
-				<div class="button">
-					<a href="<?php echo BASE_URL; ?>shirts.php">
-						<h2>Hey, I&rsquo;m Mike!</h2>
-						<p>Check Out My Shirts</p>
-					</a>
-				</div>
-			</div>
+        <img class="hero" src="<?php echo BASE_URL; ?>img/mike-the-frog.png" alt="Mike the Frog says:">
+        <div class="button">
+            <a href="<?php echo BASE_URL; ?>shirts.php">
+                <h2>Hey, I&rsquo;m Mike!</h2>
+                <p>Check Out My Shirts</p>
+            </a>
+        </div>
+    </div>
 
-		</div>
+</div>
 
-		<div class="section shirts latest">
+<div class="section shirts latest">
 
-			<div class="wrapper">
+    <div class="wrapper">
 
-				<h2>Mike&rsquo;s Latest Shirts</h2>
+        <h2>Mike&rsquo;s Latest Shirts</h2>
 
-				<?php include(ROOT_PATH . "inc/products.php"); ?>
-				<ul class="products">
-					<?php 
+        <ul class="products">
+            <?php
+            $list_view_html = "";
+            foreach ($products as $product) {
+                $list_view_html = get_list_view_html($product) . $list_view_html;
+            }
+            echo $list_view_html;
+            ?>								
+        </ul>
 
-						$total_products = count($products);
-						$position = 0;
-						$list_view_html = "";
-						foreach($products as $product_id => $product) { 
-							$position = $position + 1;
-							if ($total_products - $position < 4) {
-								$list_view_html = get_list_view_html($product_id,$product) . $list_view_html;
-							}
-						}
-						echo $list_view_html;
-					?>								
-				</ul>
+    </div>
 
-			</div>
-
-		</div>
+</div>
 
 <?php include(ROOT_PATH . 'inc/footer.php') ?>
